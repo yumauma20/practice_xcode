942f068e500e246d3a69fd6914c7f91582336827
yumauma20 <71yuma10@gmail.com>
2021-01-26 23:32:53 +0900

    Initial Commit

---
+++ test0126/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test0126
+//
+//  Created by 内藤佑馬 on 2021/01/26.
+//  Copyright © 2021 内藤佑馬. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ test0126Tests/test0126Tests.swift
@@ -0,0 +1,34 @@
+//
+//  test0126Tests.swift
+//  test0126Tests
+//
+//  Created by 内藤佑馬 on 2021/01/26.
+//  Copyright © 2021 内藤佑馬. All rights reserved.
+//
+
+import XCTest
+@testable import test0126
+
+class test0126Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
