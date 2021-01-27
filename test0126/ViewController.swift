9183709b2c9ea6b0fb4cee7e90216b410316b241
yumauma20 <71yuma10@gmail.com>
2021-01-27 17:54:59 +0900

    firstcommit

--- test0126/ViewController.swift
+++ test0126/ViewController.swift
@@ -9,12 +9,58 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var hello: UILabel!
+    @IBOutlet weak var textname: UITextField!
+    @IBOutlet weak var UIswitch: UISwitch!
+    @IBOutlet weak var humanText: UITextField!
+    
+    let humans: [String] = ["内藤","佐藤","山田","国枝","高井"]
+    
+    var btnTimer: Timer!
+    
+    func startTimer() {
+        self.btnTimer = Timer.scheduledTimer(
+            timeInterval: 0.01,
+            target: self,
+            selector: #selector(self.changeHuman),
+            userInfo: nil,
+            repeats: true
+        )
+    }
+    
+    @objc func changeHuman() {
+        var human: String!
+        human = humans.randomElement()
+        humanText.text = human
+    }
+    
+    @IBAction func start(_ sender: Any) {
+        startTimer()
+    }
+       
+    @IBAction func stop(_ sender: Any) {
+        btnTimer!.invalidate()
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnsend(_ sender: Any) {
+        hello.text = "hello " + textname.text!
+    }
+    
+    @IBAction func touch(_ sender: Any) {
+        if(UIswitch.isOn){
+            hello.textColor = UIColor.red
+        }else{
+            hello.textColor = UIColor.blue
+        }
+    }
+    
+    
 }
 
